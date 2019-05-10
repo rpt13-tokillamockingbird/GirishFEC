ee5de0ce0e23729c1c9b984e952500f69413e623
Girish Venkataraman <venkg01@gmail.com>
2019-05-09 23:44:45 -0400

    add faker first step

--- db/seedDb.js
+++ db/seedDb.js
@@ -1,9 +1,13 @@
-var db = require("../db");
+// var db = require("../db");
 var faker = require('faker');
 
 const seedData = function() {
 
-
+  const sent = faker.lorem.sentence();
+  console.log(sent);
+  console.log(faker.lorem.sentences());
 };
+
+seedData();
 
 module.exports.seedData = seedData;
